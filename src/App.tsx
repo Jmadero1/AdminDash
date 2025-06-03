d4995133369f9f6d603640a60dc75f756e18a9e6
jmadero1 <veraenriquej98@gmail.com>
2025-06-03 09:18:40 -0300

    feature: ui ready

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,10 @@ function App() {
       <SidebarProvider>
         <AppSidebar variant="inset" />
         <SidebarInset>
+          <div className="flex flex-1 flex-col pl-64">
           <SiteHeader />
-          <div className="flex flex-1 flex-col">
+          </div>
+          <div className="flex flex-1 flex-col pl-64">
             <div className="@container/main flex flex-1 flex-col gap-2">
               <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
                 <SectionCards />
